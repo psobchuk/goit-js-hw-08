b052d78edffa49c24a1783345b685747a64b9c46
Petro Sobchuk <sobchuk.p@gmail.com>
2023-01-08 21:00:05 +0200

    Modifies 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = buildGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function buildGallery(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+
+  return markup;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox');
 
 console.log(galleryItems);
 console.log(SimpleLightbox);
