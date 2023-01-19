20c89c5697fdb40e0975b18294f0439229dff267
Petro Sobchuk <sobchuk.p@gmail.com>
2023-01-19 20:56:37 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,23 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-console.log(Player);
-const test = 10;
-console.log(test);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+playOnPause();
+
+function onPlay(seconds) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(seconds));
+}
+
+function playOnPause() {
+  const pause = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!localStorage.getItem(STORAGE_KEY)) {
+    return;
+  }
+  player.setCurrentTime(pause.seconds);
+}
