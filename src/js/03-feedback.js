ec5a9ff6b68c6e5d57c8deace31c37ad1a2f987e
Petro Sobchuk <sobchuk.p@gmail.com>
2023-01-19 21:42:59 +0200

    03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+fillForm();
+
+// Відстежуємо подію input
+function onInput() {
+  const formFieldsValues = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formFieldsValues));
+}
+
+// Заповнюємо поля форми даними зі сховища
+function fillForm() {
+  const values = localStorage.getItem(STORAGE_KEY);
+
+  if (!values) {
+    return;
+  }
+
+  const { email, message } = JSON.parse(values);
+
+  form.elements.email.value = email;
+  form.elements.message.value = message;
+}
+
+// Відстежуємо сабміт форми
+function onSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+
+  console.log(data);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  return form.reset();
+}
